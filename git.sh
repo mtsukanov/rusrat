fd21789bedb3689a37cbb13530da8f149be65b9d
root <root@rusrat.sas.com>
2015-12-28 16:28:56 +0300

    -q

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 cd /var/beacon/clr
 sudo git add . 
-git commit -a -m -q "fuck"
-git tag "auto"
-git push https://github.com/mtsukanov/rusrat.git master
+sudo git commit -m -q 
+
+sudo git push https://github.com/mtsukanov/rusrat.git master
 
