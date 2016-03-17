1102a4e4e140455c5e75c0861af030d00105a81a
mtsukanov <mtsukanov@rusrat.sas.com>
2016-03-17 16:53:18 +0300

    timestamp

--- scripts/transgen.py
+++ scripts/transgen.py
@@ -30,24 +30,27 @@ def transgen():
     global k
     global fullarr
     try:
-        transid = randint(1,1000)
-        cardid = randint(1,8)
-        cardnumber = randint(10**15,10**16-1)
-        accountid = randint(1,100)
-        terminalid = randint(1,100)
-        terminaltype = choice(['atm','pos','mobapp','onlinebank'])
-        mcc = randint(1000,9999)
-        transstatus = choice(['ok','refusal','error'])
-        transdate = strftime("%d.%m.%Y %H:%M:%S",gmtime())
-        transsum = randint(100,20000)
-        transcur = choice(['rub','eur','usd','kzt'])
-        transtype = randint(0,5)
-        transinfo = choice(['proddetlimit','atmerror','cardcashlimit'])
-        fulltrans = {'transactionid':transid,'cardid':cardid,'cardnumber':cardnumber,'accountid':accountid,'terminalid':terminalid,'terminaltype':terminaltype,'mcc':mcc,
+        i==1
+        while i = 1:
+            transid = randint(1,1000)
+            cardid = randint(1,8)
+            cardnumber = randint(10**15,10**16-1)
+            accountid = randint(1,100)
+            terminalid = randint(1,100)
+            terminaltype = choice(['atm','pos','mobapp','onlinebank'])
+            mcc = randint(1000,9999)
+            transstatus = choice(['ok','refusal','error'])
+            transdate = strftime("%d.%m.%Y %H:%M:%S",gmtime())
+            transsum = randint(100,20000)
+            transcur = choice(['rub','eur','usd','kzt'])
+            transtype = randint(0,5)
+            transinfo = choice(['proddetlimit','atmerror','cardcashlimit'])
+            fulltrans = {'transactionid':transid,'cardid':cardid,'cardnumber':cardnumber,'accountid':accountid,'terminalid':terminalid,'terminaltype':terminaltype,'mcc':mcc,
 'transactionstatus':transstatus,'transactiondate':transdate,'transactionsum':transsum,'transactioncurrency':transcur,'transactiontype':transtype,
 'transactioninfo':transinfo}
-        que_result = rabbitmq_add.delay('trans_mq','t_mq',json.dumps(fulltrans,ensure_ascii=False),'application/json','trans_mq')
-        return fulltrans
+            que_result = rabbitmq_add.delay('trans_mq','t_mq',json.dumps(fulltrans,ensure_ascii=False),'application/json','trans_mq')
+            fullarr.append(fulltrans)
+        return fullarr
     except Exception as e:
         return e
 
