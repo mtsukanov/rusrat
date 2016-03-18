a4323f5c85e27fa70146e4932269ed911a0720d6
mtsukanov <mtsukanov@rusrat.sas.com>
2016-03-18 16:25:43 +0300

    timestamp

--- scripts/transgen.py
+++ scripts/transgen.py
@@ -5,7 +5,7 @@ from random import randint,choice
 from celery import Celery
 from flask import json
 
-app = Celery(backend='amqp://',broker='redis://localhost/0', celery_event_queue_ttl = 300)
+app = Celery(broker='redis://localhost/0', celery_event_queue_ttl = 300)
 """broker='amqp://guest:guest@localhost:5672//'"""
 app.conf.update(CELERY_RESULT_BACKEND='amqp')
 @app.task
