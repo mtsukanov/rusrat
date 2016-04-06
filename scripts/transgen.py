22ada03682995112ed1d988c9a83dcb4e04b6837
mtsukanov <mtsukanov@rusrat.sas.com>
2016-04-06 16:34:10 +0300

    timestamp

--- scripts/transgen.py
+++ scripts/transgen.py
@@ -32,7 +32,7 @@ def transgen():
     try:
         i=0
         fullarr = []
-        conn = pymssql.connect(server = '172.28.106.17',user = 'rtdm',password = 'Orion123',database='CIDB')
+        conn = pymssql.connect(server = '172.28.106.17',user = 'sa',password = 'Orion123',database='CIDB')
         cursor = conn.cursor()
         cursor.execute('SELECT * FROM TRANSData.TRANSACTION')
         data = cursor.fetchall()
