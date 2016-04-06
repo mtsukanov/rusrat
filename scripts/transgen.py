733aba7ed49b8d452a0f62ed9390b05ae2daab5d
mtsukanov <mtsukanov@rusrat.sas.com>
2016-04-06 16:31:42 +0300

    timestamp

--- scripts/transgen.py
+++ scripts/transgen.py
@@ -32,9 +32,9 @@ def transgen():
     try:
         i=0
         fullarr = []
-        conn = pymssql.connect(server = '172.28.106.245',user = 'rtdm',password = 'Orion123',database='CIDB')
+        conn = pymssql.connect(server = '172.28.106.17',user = 'rtdm',password = 'Orion123',database='CIDB')
         cursor = conn.cursor()
-        cursor.execute('SELECT max(TransID) FROM TRANSData.TRANSACTIONS')
+        cursor.execute('SELECT * FROM TRANSData.TRANSACTIONS')
         data = cursor.fetchall()
         print data
         while i ==0 :
