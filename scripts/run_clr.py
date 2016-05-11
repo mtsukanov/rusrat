00afe78d693b1da7ba091791c37f63517851ae5c
mtsukanov <mtsukanov@rusrat.sas.com>
2016-05-11 19:35:01 +0300

    timestamp

--- scripts/run_clr.py
+++ scripts/run_clr.py
@@ -810,7 +810,7 @@ def offer_accept():
     #result = call_rtdm("172.28.106.245","responsehistoryevent",inputs)
     try:
         blat = call_rtdm("172.28.106.245","responsehistoryevent",inputs).delay()
-        return make_response(jsonify(str(blat)),201)
+        return make_response(jsonify('ok'),201)
     except Exception as e:
         return make_response(jsonify({'Ratatoskr':e}),418)  
     #return make_response(jsonify(result),201)
