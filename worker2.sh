6586f171f3240f2bc8818294b8a82eb8cc9c2429
mtsukanov <mtsukanov@rusrat.sas.com>
2016-05-11 20:10:36 +0300

    timestamp

--- worker2.sh
+++ worker2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash   
 source /var/beacon/clr/bin/activate
 cd /var/beacon/clr/scripts
-celery -A transgen worker --loglevel=DEBUG --concurrency=5 -n worker2.%h 
 celery -A ctasks worker --loglevel=DEBUG --concurrency=5 -n worker2.%h 
 
 
--- worker2.sh~
+++ worker2.sh~
@@ -2,7 +2,7 @@
 source /var/beacon/clr/bin/activate
 cd /var/beacon/clr/scripts
 celery -A transgen worker --loglevel=DEBUG --concurrency=5 -n worker2.%h 
-
+celery -A ctasks worker --loglevel=DEBUG --concurrency=5 -n worker2.%h 
 
 
 
