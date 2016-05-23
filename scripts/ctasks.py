4f2358e7a74e123b82dc55d65f9f9e6a53ec5264
mtsukanov <mtsukanov@rusrat.sas.com>
2016-05-23 18:31:19 +0300

    timestamp

--- scripts/ctasks.py
+++ scripts/ctasks.py
@@ -137,7 +137,7 @@ def post(maxevent):
         cur.execute(query)
         for row in cur.fetchall():
             inputs = {"IndivID":row[0],"Channel":"Luna","FotoDT":"1992-10-02","param1":"","param2":"","param3":"","param4":""}
-            call_rtdm("172.28.106.245","scoringevent",inputs)
+            k = call_rtdm("172.28.106.245","scoringevent",inputs)
             Out.append({"event_id":row[0],"event_time":row[1],"similarity":row[2],"first_name":row[3],"last_name":row[4]})
         query2 = "SELECT MAX(event_id) FROM event"
         cur.execute(query2)
@@ -145,7 +145,7 @@ def post(maxevent):
         maxid = data[0]
         #data = cur.fetchall()
         #cnt = int(data[0][0])
-        print Out,maxid,resp
+        print Out,maxid,k
 
 
 
