7b09e6241381b0440392e1722582c81305f186d3
mtsukanov <mtsukanov@rusrat.sas.com>
2016-05-23 19:04:26 +0300

    timestamp

--- scripts/ctasks.py
+++ scripts/ctasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from celery.task.control import inspect
 from time import sleep
+from time import gmtime, strftime,strptime
 from flask import json
 import pika
 import requests
