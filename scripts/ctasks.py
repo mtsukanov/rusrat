ecfe5bcb1f7529d18421290d2476ae58b41be91d
mtsukanov <mtsukanov@rusrat.sas.com>
2016-06-03 18:52:53 +0300

    timestamp

--- scripts/ctasks.py
+++ scripts/ctasks.py
@@ -2,9 +2,8 @@ from celery import Celery
 from celery.task.control import inspect
 from time import sleep
 from time import gmtime, strftime,strptime
-#from datetime import timedelta,datetime
+from datetime import timedelta,datetime
 from flask import json
-import datetime
 import pika
 import requests
 import MySQLdb
@@ -185,9 +184,9 @@ def post(maxevent):
                 #inputs = {"IndivID":int(row[5]),"Channel":"Luna","PhotoDT":str(row[1].isoformat(sep='T')),"param1":"","param2":"","param3":0,"param4":0}
                 #k = call_rtdm("172.28.106.245","lunaevent",inputs)
                 #print k,inputs
-            date_s = datetime.datetime.strftime(datetime.datetime.now(),"%m/%d/%y %H:%M:%S")
-            date_e = datetime.datetime.strftime(lasttimereq,"%m/%d/%y %H:%M:%S")
-            print date_s-date_e
+            date_s = datetime.strftime(datetime.now(),"%m/%d/%y %H:%M:%S")
+            date_e = datetime.strftime(lasttimereq,"%m/%d/%y %H:%M:%S")
+            print (datetime.now()-lasttimereq).seconds
             if datetime.now() - lasttimereq >= timedelta(minutes=5) and row[2] > 85.00:
                 payload3 = {"cid":int(row[5]),"scenario":"","beaconid":"","spotid":2,"spotname":"The Store","time":str(datetime.now().isoformat(sep='T')),"trigger":"Luna"}
                 #r3 = requests.post("http://172.28.104.171:5000/geotrigger",json = payload3)
