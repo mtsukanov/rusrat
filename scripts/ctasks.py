5ab79e3dad5eef28652d70f45718800e3fef09c0
mtsukanov <mtsukanov@rusrat.sas.com>
2016-06-03 18:43:42 +0300

    timestamp

--- scripts/ctasks.py
+++ scripts/ctasks.py
@@ -185,7 +185,7 @@ def post(maxevent):
                 #k = call_rtdm("172.28.106.245","lunaevent",inputs)
                 #print k,inputs
             date_s = datetime.strftime(datetime.now(),"%m/%d/%y %H:%M:%S")
-            date_e = lasttimereq
+            date_e = datetime.strftime(lasttimereq,"%m/%d/%y %H:%M:%S")
             print date_e
             if datetime.now() - lasttimereq >= timedelta(minutes=5) and row[2] > 85.00:
                 payload3 = {"cid":int(row[5]),"scenario":"","beaconid":"","spotid":2,"spotname":"The Store","time":str(datetime.now().isoformat(sep='T')),"trigger":"Luna"}
