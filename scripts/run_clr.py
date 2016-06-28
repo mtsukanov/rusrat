64d9340665325ee4bda44f97bd72b89cf6052788
mtsukanov <mtsukanov@rusrat.sas-mic.sas>
2016-06-28 18:21:13 +0300

    timestamp

--- scripts/run_clr.py
+++ scripts/run_clr.py
@@ -36,7 +36,7 @@ import pymssql
 import psycopg2
 import urllib
 #import transgen
-from proj.celery import app
+from celery import app
 #############################################################################################################################################################################################
 #                                                                                                                                                                                           #
 #                         BLOCK OF GLOBAL VARIABLES                                                                                                                                         #
@@ -625,7 +625,7 @@ def facetzmanage():
         return make_response(jsonify({'Ratatoskr':'FacetZ service has been enabled'}),200)
     else:
         for k,v in facetzstack.iteritems():
-            revoke(facetzstack[k], terminate = True)
+            app.control.revoke(facetzstack[k])
         facetz_enable = False
         ServicesStatusPost('facetz',False)
         return make_response(jsonify({'Ratatoskr':'FacetZ service has been disabled'}),201)
