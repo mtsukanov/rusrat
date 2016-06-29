2ad16eda5392323fc780a55456187de3912cc058
mtsukanov <mtsukanov@rusrat.sas-mic.sas>
2016-06-29 12:10:33 +0300

    timestamp

--- scripts/run_clr.py
+++ scripts/run_clr.py
@@ -625,7 +625,7 @@ def facetzmanage():
         return make_response(jsonify({'Ratatoskr':'FacetZ service has been enabled'}),200)
     else:
         for k,v in facetzstack.iteritems():
-            AsyncResult(facetzstack[k]).revoke()
+            AsyncResult(facetzstack[k]).revoke(terminate=True)
         facetz_enable = False
         ServicesStatusPost('facetz',False)
         return make_response(jsonify({'Ratatoskr':'FacetZ service has been disabled'}),201)
