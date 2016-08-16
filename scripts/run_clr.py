ec02644debc605f7a8f09643a351251ff884190c
mtsukanov <mtsukanov@rusrat.sas-mic.sas>
2016-08-16 11:26:10 +0300

    timestamp

--- scripts/run_clr.py
+++ scripts/run_clr.py
@@ -505,13 +505,16 @@ def deco():
     if param == 'True': 
         #time.sleep(3)
         maxid = get_max_eventid_luna()
-        resultcam = post.apply_async([maxid])    
+        resultcam = post.apply_async([maxid])   
+        bool_tmp = dur.set('resultcam',resultcam) 
         ServicesStatusPost('luna',True)
-        return make_response(jsonify({'Cameracheck':'Task '+str(resultcam)+' has been added to Redis'}),200)
+        #return make_response(jsonify({'Cameracheck':'Task '+str(resultcam)+' has been added to Redis'}),200)
+        return make_response(jsonify({'Cameracheck':'Task '+dur.get('resultcam')+' has been added to Redis'}),200)
     else:
-        resultcam.revoke(terminate=True) 
+        AsyncResult(id=dur.get('resultcam')).revoke(terminate=True) 
         ServicesStatusPost('luna',False)
-        return make_response(jsonify({'Cameracheck':'Task '+str(resultcam)+' has been terminated'}),200)
+        #return make_response(jsonify({'Cameracheck':'Task '+str(resultcam)+' has been terminated'}),200)
+        return make_response(jsonify({'Cameracheck':'Task '+dur.get('resultcam')+' has been terminated'}),200)
 
 
 #############################################################################################################################################################################################
